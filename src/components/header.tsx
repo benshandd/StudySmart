25456583bf0245dac549299a9b00b22f7f3b89a5
Ben Shand <benjaminshand101@gmail.com>
2024-07-02 13:37:33 +1200

    Some styling for starting quiz

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,10 +3,11 @@ import Link from "next/link";
 const Header = () => {
   return(
     <header>
-      <nav>
-        <Link href={"/quiz"}>Sample Quiz</Link>
-        <Link href={"/quiz/new"}>New Quiz</Link>
+      <nav className="px-4 py-2.5 flex gap-2">
+        <Link className="underline" href={"/quiz"}>Sample Quiz</Link>
+        <Link className="underline" href={"/quiz/new"}>New Quiz</Link>
       </nav>
     </header>
   )
 };
+export default Header
